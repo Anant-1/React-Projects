dddd1d5f0716e2826964c25fb14fa1f2157d3ec6
Anant-1 <54532485+Anant-1@users.noreply.github.com>
2023-12-22 02:44:01 +0530

    Learn Redux Toolkit

---
+++ ReactReduxTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+function App() {
+  return (
+    <>
+      <h1>Learning Redux</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
